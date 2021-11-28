5eb9724a2098c6429d743d7356465e6b2a990f86
ivarprudnikov <ivar.prudnikov@gmail.com>
2021-11-28 11:45:53 +0000

    include apache2 and php in the installation script

--- node-agent/install.sh
+++ node-agent/install.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -eu
 
+# install php server and overwrite the default index.html
+apt update
+DEBIAN_FRONTEND=noninteractive apt -y install apache2 php7.2 libapache2-mod-php7.2
+mv /var/www/html/index.html /var/www/html/index.php
+echo '<?php header("Content-Type: text/plain"); echo "Hello, world!\n"; ?>' > /var/www/html/index.php
+chmod 0644 /var/www/html/index.php
+chown nobody:nogroup /var/www/html/index.php
+service apache2 start
+
 # Create a working directory
 mkdir -p /var/configwatcher
 # Copy a config watcher script
